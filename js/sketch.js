cfa3d5366db1fd79cf5e2f442726b886245968ac
Arpit Jain <abs.jain16@gmail.com>
2020-09-13 03:13:28 +0530

    Set up canvas

---
+++ js/sketch.js
@@ -0,0 +1,12 @@
+/**
+ * sketch.js - containing 'setup' and 'draw' funtions for
+ *              p2.js along with other bootstraping variables
+ */
+
+function setup() {
+    createCanvas(640, 360);
+}
+
+function draw() {
+    background(64);
+}
